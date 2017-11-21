4bc54f2365396ae9d7e7b405fde74e060782efce
thorgeo <thorgeo65@gmail.com>
2017-11-21 14:27:45 +0900

    tmp

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
+import 'babel-polyfill'
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Provider, connect } from 'react-redux'
+import request from 'superagent'
+import createSagaMiddleware from "redux-saga"
+import { call, put, fork, takeEvery } from 'redux-saga/effects';
 
 // React component
 class Counter extends Component {
   render() {
-    const { value, onIncreaseClick } = this.props
+    const { value, onIncreaseClick, onDecreaseClick } = this.props
     return (
       <div>
         <span>{value}</span>
         <button onClick={onIncreaseClick}>Increase</button>
+        <button onClick={onDecreaseClick}>Decrease</button>
       </div>
     )
   }
@@ -22,8 +27,30 @@ Counter.propTypes = {
   onIncreaseClick: PropTypes.func.isRequired
 }
 
-// Action
+class Clock extends Component {
+  render() {
+    const { time, onClick } = this.props
+
+    return(
+      <div>
+        <span>{time}</span>
+        <button onClick={onClick}>GET CURRENT TIME</button>
+      </div>
+    )
+  }
+}
+
+Clock.propTypes = {
+  time: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+}
+
+// Counter Action
 const increaseAction = { type: 'increase' }
+const decreaseAction = { type: 'decrease' }
+
+// Clock Action
+const getCurrentTimeAction = { type: 'getjson' }
 
 // Reducer
 function counter(state = { count: 0 }, action) {
@@ -31,33 +58,112 @@ function counter(state = { count: 0 }, action) {
   switch (action.type) {
     case 'increase':
       return { count: count + 1 }
+    case 'decrease':
+      return { count: count - 1 }
     default:
       return state
   }
 }
 
-// Store
-const store = createStore(counter)
+function clock(state = { time: 'YYYY-MM-DD' }, action) {
+  switch (action.type) {
+    case 'getjson':
+      console.log(action)
+      return { time: time }
+    default:
+      return state
+  }
+}
+
+async function callAPI() {
+  const response = await request.get('http://date.jsontest.com/')
+  return response
+}
+
+
+const reducer = combineReducers({
+                  clock,
+                  counter
+                })
+
+// Store with Sage
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  reducer,
+  {},
+  applyMiddleware(sagaMiddleware)
+);
+
+function checkoutSuccess(body) {
+  return {
+    type: 'getjson',
+    body
+  }
+}
+
+function* runCurrnetTimeRequest(action) {
+  const response = yield call(request.get, 'http://date.jsontest.com/')
+  console.log(response)
+  yield put(checkoutSuccess(body))
+}
+
+function* handleCurrentTime() {
+  yield takeEvery('getjson', runCurrentTimeRequest)
+}
+
+function* rootSaga() {
+  //yield fork(handleCurretTime)
+}
+
+sagaMiddleware.run(rootSaga)
+
 
 // Map Redux state to component props
-function mapStateToProps(state) {
+function mapCounterStateToProps(state) {
   return {
-    value: state.count
+    value: state.counter.count
   }
 }
 
 // Map Redux actions to component props
-function mapDispatchToProps(dispatch) {
+function mapCounterDispatchToProps(dispatch) {
   return {
-    onIncreaseClick: () => dispatch(increaseAction)
+    onIncreaseClick: () => dispatch(increaseAction),
+    onDecreaseClick: () => dispatch(decreaseAction)
   }
 }
 
-// Connected Component
-const App = connect(
-  mapStateToProps,
-  mapDispatchToProps
+// Map Redux state to component props
+function mapClockStateToProps(state) {
+  return {
+    time: state.clock.time
+  }
+}
+
+// Map Redux actions to component props
+function mapClockDispatchToProps(dispatch) {
+  return {
+    onClick: () => dispatch(getCurrentTimeAction)
+  }
+}
+
+// Connected Component == Container
+const CounterContainer = connect(
+  mapCounterStateToProps,
+  mapCounterDispatchToProps
 )(Counter)
+
+const ClockContainer = connect(
+  mapClockStateToProps,
+  mapClockDispatchToProps
+)(Clock)
+
+const App = () => (
+  <div>
+    <CounterContainer />
+    <ClockContainer />
+  </div>
+)
 
 ReactDOM.render(
   <Provider store={store}>
